30f901e4839548e832f55f0a6b0ad6520ccecd6d
DwiAulia76 <dwiaulia0764@gmail.com>
2024-07-02 14:34:00 +0700

    navbar

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
-// ini js
+// hamburger button
+document.addEventListener('DOMContentLoaded', function () {
+    const toggleButton = document.querySelector('.toggle-button');
+    const nav = document.querySelector('nav');
+
+    toggleButton.addEventListener('click', function () {
+        this.classList.toggle('active');
+        nav.classList.toggle('active');
+    });
+});
