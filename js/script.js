fc82a137d5fedffa29e764f14efcb15a7cc3bd6c
DwiAulia76 <dwiaulia0764@gmail.com>
2024-07-04 17:00:58 +0700

    final

--- js/script.js
+++ js/script.js
@@ -1,5 +1,39 @@
 
+// navbar
 
+// banner home
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+// Manually call showSlides function to display the first slide
+showSlides(slideIndex);
+// submit form
 function submitForm() {
     // value input
     var nama = document.getElementById("nama").value;
